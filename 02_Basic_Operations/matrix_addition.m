e444c4c379bd10006a6ea1267f4412bc35bdd5bf
Navid <50656059+naviiidz@users.noreply.github.com>
2024-08-13 13:09:53 -0400

    Create matrix_addition.m

---
+++ 02_Basic_Operations/matrix_addition.m
@@ -0,0 +1,18 @@
+% Matrix Addition
+
+clc;
+clear;
+
+% Define two matrices
+A = [1, 2, 3; 4, 5, 6; 7, 8, 9];
+B = [9, 8, 7; 6, 5, 4; 3, 2, 1];
+
+% Add matrices
+C = A + B;
+
+disp('Matrix A:')
+disp(A)
+disp('Matrix B:')
+disp(B)
+disp('A + B =')
+disp(C)
