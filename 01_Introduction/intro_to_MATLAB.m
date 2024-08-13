8ecfd9f3fcc690b92f90695b04ec01dbe6e89bd8
Navid <50656059+naviiidz@users.noreply.github.com>
2024-08-13 13:09:03 -0400

    Create intro_to_MATLAB.m

---
+++ 01_Introduction/intro_to_MATLAB.m
@@ -0,0 +1,14 @@
+% Introduction to MATLAB
+% This script provides a basic introduction to the MATLAB environment.
+
+clc;
+clear;
+
+% Displaying text in MATLAB
+disp('Welcome to MATLAB!')
+
+% Simple calculation
+a = 5;
+b = 3;
+c = a + b;
+disp(['The result of 5 + 3 is: ', num2str(c)])
