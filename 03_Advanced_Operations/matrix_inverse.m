ae801bfb034ce8a141948e3550bc6d1a66b1bd8e
Navid <50656059+naviiidz@users.noreply.github.com>
2024-08-13 13:10:13 -0400

    Create matrix_inverse.m

---
+++ 03_Advanced_Operations/matrix_inverse.m
@@ -0,0 +1,15 @@
+% Matrix Inverse
+
+clc;
+clear;
+
+% Define a matrix
+A = [1, 2; 3, 4];
+
+% Calculate the inverse
+A_inv = inv(A);
+
+disp('Matrix A:')
+disp(A)
+disp('Inverse of A:')
+disp(A_inv)
